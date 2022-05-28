0ee020dff30c379a494d9b1773b7d17db99a5970
Ihor Baklazhko <ihor.b@arringo.com>
2022-05-28 17:04:16 +0200

    updated main() by adding the cycle, which checks the guesses

--- main.cpp
+++ main.cpp
@@ -1,8 +1,36 @@
 #include "wordle.h"
 
+#include <iostream>
+
 int main()
 {
   Wordle wordle("dict.txt");
 
+  int tries{6};
+  std::string word;
+
+  while (tries--)
+  {
+      std::cout << "Please enter the guess word: ";
+      std::cin >> word;
+
+      // need a word validation here
+
+      const auto& resp = wordle.guess(word);
+
+      if (resp.first)
+      {
+          std::cout << "Congrats! You guessed the word :)\n";
+      }
+      else
+      {
+          std::cout << "Bad luck, please hit enter to try again\n";
+      }
+
+      std::cout << resp.second << std::endl;
+
+      // wait until enter is pressed
+      std::cin.get();
+  }
   // ...
 }
